0960f66c7d81f2f4d99ae968a07ddab9f6dbee8f
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-06 03:38:07 +0500

    New Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <>
-      
-    </>
+    <div className='flex h-screen bg-gray-100 text-gray-100 overflow-hidden'>
+      hello
+    </div>
   )
 }
 
