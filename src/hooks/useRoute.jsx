0c5b0192c90a47cc361af97445cc8a28dc650890
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-06 04:26:38 +0500

    New Commit

--- src/hooks/useRoute.jsx
+++ src/hooks/useRoute.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
 import { PATH } from "./usePath";
 import { Analytics, Orders, Overview, Products, Sales, Settings, Users } from '../pages';
+import { BarChart2, DollarSign, Settings2, ShoppingBag, ShoppingCart, TrendingUp, Users2 } from 'lucide-react';
 
-
+export const sidebarLinks = [
+    {
+        id:1,
+        icon:<BarChart2/>,
+        title:"Overview",
+        color:"#6366f1",
+        link:PATH.overview
+    },
+    {
+        id:2,
+        icon:<ShoppingBag/>,
+        title:"Products",
+        color:"#8B5CF6",
+        link:PATH.products
+    },
+    {
+        id:3,
+        icon:<Users2/>,
+        title:"Users",
+        color:"#EC4899",
+        link:PATH.users
+    },
+    {
+        id:4,
+        icon:<DollarSign/>,
+        title:"Sales",
+        color:"#10B981",
+        link:PATH.sales
+    },
+    {
+        id:5,
+        icon:<ShoppingCart/>,
+        title:"Orders",
+        color:"#F59E0B",
+        link:PATH.orders
+    },
+    {
+        id:6,
+        icon:<TrendingUp/>,
+        title:"Analytics",
+        color:"#3B82F6",
+        link:PATH.analytics
+    },
+    {
+        id:7,
+        icon:<Settings2/>,
+        title:"Settings",
+        color:"#6EE7B7",
+        link:PATH.settings
+    },
+]
 export const pagesRoute = [
     {
         id:1,
