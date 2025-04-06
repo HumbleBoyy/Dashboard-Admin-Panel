1dbc0817a29e96fb28d513d8dfca0fcab87311f2
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-06 06:01:46 +0500

    New Commit

--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     const {pathname} = useLocation()
   return (
     <header className='w-full mx-auto py-4 px-4 sm:px-6 lg:px-8 bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg border-b border-gray-700'>
-        {sidebarLinks.map((item) => item.link === pathname ? <h1>{item.title}</h1> : null)}
+        {sidebarLinks.map((item) => item.link === pathname ? <h1 className='text-2xl font-semibold text-gray-100'>{item.title}</h1> : null)}
     </header>
   )
 }
