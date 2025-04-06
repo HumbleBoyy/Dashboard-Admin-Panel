b7542a77b85e6e32f82e799d2ff023d299c82d25
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-06 06:00:35 +0500

    New Commit

--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
+import { sidebarLinks } from '../hooks/useRoute'
 
 const Header = () => {
+    const {pathname} = useLocation()
   return (
     <header className='w-full mx-auto py-4 px-4 sm:px-6 lg:px-8 bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg border-b border-gray-700'>
-        <h1></h1>
+        {sidebarLinks.map((item) => item.link === pathname ? <h1>{item.title}</h1> : null)}
     </header>
   )
 }
