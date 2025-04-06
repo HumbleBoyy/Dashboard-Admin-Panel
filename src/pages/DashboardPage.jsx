a4e99f48bdb154f9ed744418d4e83c3f64b7c29e
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-06 05:45:04 +0500

    New COmmit

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { pagesRoute } from '../hooks/useRoute'
+import Header from '../modules/Header'
 
 const DashboardPage = () => {
   return (
-    <Routes>
-        {pagesRoute.map((item)=> <Route path={item.path} element={item.element} key={item.id}/>)}
-    </Routes>
+    <div className='flex flex-col'>
+      <Header/>
+      <Routes>
+          {pagesRoute.map((item)=> <Route path={item.path} element={item.element} key={item.id}/>)}
+      </Routes>
+    </div>
   )
 }
 
