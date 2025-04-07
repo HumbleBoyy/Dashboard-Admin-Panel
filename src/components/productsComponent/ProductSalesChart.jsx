ff8673c247d4183330b34af32f8de6c7017acfb7
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-07 21:51:12 +0500

    New COmmit

--- src/components/productsComponent/ProductSalesChart.jsx
+++ src/components/productsComponent/ProductSalesChart.jsx
@@ -18,6 +18,30 @@ const ProductSalesChart = () => {
     transition={{ delay: 0.3 }}
 >
 <h2 className='text-xl font-semibold text-gray-100 mb-4'>Sales Trend</h2>
+    <div className='h-80'>
+        <ResponsiveContainer width={"100%"} height={"100%"}>
+                    <LineChart data={salesData}>
+                    <CartesianGrid strokeDasharray={"3 3"} stroke='#4B5563'/>
+                    <XAxis dataKey={"month"} stroke='#9ca3af'/>
+                    <YAxis stroke='#9ca3af'/>
+                    <Tooltip 
+                    contentStyle={{
+                        backgroundColor: "rgba(31, 41, 55, 0.8)",
+                        borderColor: "#4B5563",
+                    }}
+                    itemStyle={{ color: "#E5E7EB" }}
+                    />
+                        <Line
+                            type='monotone'
+                            dataKey='sales'
+                            stroke='#6366F1'
+                            strokeWidth={3}
+                            dot={{ fill: "#6366F1", strokeWidth: 2, r: 6 }}
+                            activeDot={{ r: 8, strokeWidth: 2 }}
+                            />
+                    </LineChart> 
+        </ResponsiveContainer>
+    </div>
 </motion.div>
   )
 }
