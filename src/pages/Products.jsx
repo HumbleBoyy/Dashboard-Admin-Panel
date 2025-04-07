8875b7e94951ab375ce2c910c4c78334e9385a8a
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-07 21:36:27 +0500

    New Commit

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion';
-import { BarChart2, ShoppingBag, Users, Zap } from "lucide-react";
+import { AlertTriangle, BarChart2, DollarSign, Package, ShoppingBag, TrendingUp, Users, Zap } from "lucide-react";
 import StatCard from "../components/StatCard";
 const Products = () => {
   return (
@@ -12,10 +12,10 @@ const Products = () => {
          animate={{opacity:1, y:0}}
          transition={{duration:1}}
         >
-          <StatCard title='Total Sales' icon={Zap} value='$900,000' color='#6366F1' />
-					<StatCard title='New Users' icon={Users} value='4,671' color='#8B5CF6' />
-					<StatCard title='Total Products' icon={ShoppingBag} value='1890' color='#EC4899' />
-					<StatCard title='Conversion Rate' icon={BarChart2} value='15.9%' color='#10B981' />
+          <StatCard title='Total Products' icon={Package} value='$900,000' color='#6366F1' />
+					<StatCard title='Top Selling' icon={TrendingUp} value='4,671' color='#8B5CF6' />
+					<StatCard title='Low Stock' icon={AlertTriangle} value='67' color='#EC4899' />
+					<StatCard title='Total Revenue' icon={DollarSign} value='15.9%' color='#10B981' />
         </motion.div>
       </main>
     </div>
