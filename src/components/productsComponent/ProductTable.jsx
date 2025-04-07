2f1fe6fb771570cedb22e10a6e2f524a1a211c12
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-07 22:14:42 +0500

    new Commit

--- src/components/productsComponent/ProductTable.jsx
+++ src/components/productsComponent/ProductTable.jsx
@@ -18,7 +18,13 @@ const ProductTable = () => {
 			transition={{ delay: 0.2 }}
     >        
 
-    
+    <div className='flex justify-between items-center mb-6'>
+      <h2 className='text-lg font-medium text-gray-100'>Product List</h2>
+	  <div className='relative'>
+        <input type='text' placeholder='Search Products...' className='bg-gray-700 text-white placeholder-gray-400 rounded-lg pl-10 pr-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500'/>
+		<Search className='absolute left-3 top-2.5 text-gray-400' size={18} />
+	  </div>
+	</div>
     </motion.div>        
   )
 }
