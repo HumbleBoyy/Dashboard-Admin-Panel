4c663559ee19443acf7b4cac0ad089c2f2cc0ace
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-07 21:33:36 +0500

    New Commit

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import { motion } from 'framer-motion';
+import { BarChart2, ShoppingBag, Users, Zap } from "lucide-react";
+import StatCard from "../components/StatCard";
 const Products = () => {
   return (
-    <div>Products</div>
+    <div className='flex-1 overflow-auto relative'>
+       <main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
+        <motion.div
+         className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+         initial={{opacity:0, y:20}}
+         animate={{opacity:1, y:0}}
+         transition={{duration:1}}
+        >
+          <StatCard title='Total Sales' icon={Zap} value='$900,000' color='#6366F1' />
+					<StatCard title='New Users' icon={Users} value='4,671' color='#8B5CF6' />
+					<StatCard title='Total Products' icon={ShoppingBag} value='1890' color='#EC4899' />
+					<StatCard title='Conversion Rate' icon={BarChart2} value='15.9%' color='#10B981' />
+        </motion.div>
+      </main>
+    </div>
   )
 }
 
