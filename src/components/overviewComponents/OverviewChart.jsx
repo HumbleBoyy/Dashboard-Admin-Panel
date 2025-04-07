29bd29fc60a9e6539c643917588eaa00b07038d3
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-07 16:00:22 +0500

    new Commit

--- src/components/overviewComponents/OverviewChart.jsx
+++ src/components/overviewComponents/OverviewChart.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 import { motion } from "framer-motion";
-const salesData = [
+const reportSalesData = [
 	{ title: "Jul", sales: 9999 },
 	{ title: "Aug", sales: 2500 },
 	{ title: "Sep", sales: 8100 },
@@ -24,6 +24,32 @@ const OverviewChart = () => {
      transition={{delay:0.2}}
     >
      <h2 className='text-lg font-medium mb-4 text-gray-100'>Sales Overview</h2>
+
+     <div className='h-80'>
+        <ResponsiveContainer width={"100%"} height={"100%"}>
+           <LineChart data={reportSalesData}>
+             <CartesianGrid strokeDasharray={"3 3"} stroke='#4B5563'/>
+             <XAxis dataKey={"title"} stroke='#9ca3af'/>
+             <YAxis stroke='#9ca3af'/>
+             <Tooltip 
+              contentStyle={{
+                backgroundColor: "rgba(31, 41, 55, 0.8)",
+                borderColor: "#4B5563",
+            }}
+            itemStyle={{ color: "#E5E7EB" }}
+             >
+                  <Line
+                    type='monotone'
+                    dataKey='sales'
+                    stroke='#6366F1'
+                    strokeWidth={3}
+                    dot={{ fill: "#6366F1", strokeWidth: 2, r: 6 }}
+                    activeDot={{ r: 8, strokeWidth: 2 }}
+					/>
+             </Tooltip>
+           </LineChart> 
+        </ResponsiveContainer>
+     </div>
     </motion.div>
   )
 }
