e1ef7faefb8719c2ee512924623324da5119de74
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-08 21:30:40 +0500

    New Commit

--- src/components/salesComponent/SalesOverView.jsx
+++ src/components/salesComponent/SalesOverView.jsx
@@ -13,8 +13,19 @@ const monthlySalesData = [
 	{ month: "Jul", sales: 7000 },
 ];
 const SalesOverView = () => {
+    const [selectTimeTange, setSelectTimeRange] = useState("This month")
   return (
-    <div>SalesOverView</div>
+    <motion.div
+			className='bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 border border-gray-700 mb-8'
+			initial={{ opacity: 0, y: 20 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ delay: 0.2 }}
+	>
+    <div className='flex items-center justify-between mb-6'>
+      <h2 className='text-xl font-semibold text-gray-100'>Sales Overview</h2>
+    </div>
+
+    </motion.div>
   )
 }
 
