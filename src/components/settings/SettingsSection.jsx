5eab56a8ba6738ce4124077ec45d5feace727ecd
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-12 15:47:07 +0500

    New Commit

--- src/components/settings/SettingsSection.jsx
+++ src/components/settings/SettingsSection.jsx
@@ -8,10 +8,10 @@ const SettingsSection = ({icon:Icon, title, children}) => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
     >
-    <div className='flex items-center mb-4'>
-        <Icon className='text-indigo-400 mr-4' size='24' />
-        <h2 className='text-xl font-semibold text-gray-100'>{title}</h2>
-    </div>
+      <div className='flex items-center mb-4'>
+          <Icon className='text-indigo-400 mr-4' size='24' />
+          <h2 className='text-xl font-semibold text-gray-100'>{title}</h2>
+      </div>
       {children}
     </motion.div>
   )
