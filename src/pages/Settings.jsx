042decaf17e11c7eb218e1a67572e97108485d38
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-12 15:32:43 +0500

    new Commit

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Profile from '../components/settings/Profile'
 
 const Settings = () => {
   return (
-    <div>Settings</div>
+    <div className='flex-1 overflow-auto relative bg-gray-900'>
+        <main className='max-w-4xl mx-auto py-6 px-4 lg:px-8'>
+            <Profile/>
+        </main>
+    </div>
   )
 }
 
