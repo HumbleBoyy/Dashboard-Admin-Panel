3a28e5ea9d4ea734c518a655152001121d27b95f
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-12 16:07:46 +0500

    New Commit

--- src/components/settings/Profile.jsx
+++ src/components/settings/Profile.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 const Profile = () => {
   return (
     <SettingsSection icon={User} title={"Profile"}>
-        <div className='flex flex-col sm:flex-row items-center mb-6'>
+        <div className='flex flex-row items-center mb-6'>
            <img src={ProfileImage} alt="Profile" 
             width={50}
             height={50}
@@ -15,7 +15,7 @@ const Profile = () => {
            />
            <div>
               <h3 className='text-lg font-semibold text-gray-100'>Asadulloh</h3>
-              <Link target='_blank' className='text-blue-500' to={"https://asadullohs.vercel.app/"}>https://asadullohs.vercel.app/</Link>
+              <Link target='_blank' className='text-blue-500' to={"https://asadullohs.vercel.app/"}>asadullohs@gmail.com</Link>
            </div>
         </div>
         <button className='bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto'>Edit Profile</button>
