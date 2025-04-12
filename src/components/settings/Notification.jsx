0cc3e8d64f8c6b71ba8ce52e30e44d32edddb5e6
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-12 16:24:37 +0500

    New Commit

---
+++ src/components/settings/Notification.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SettingsSection from './SettingsSection'
+
+const Notification = () => {
+  return (
+    <SettingsSection>
+        
+    </SettingsSection>
+  )
+}
+
+export default Notification
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -6,6 +6,7 @@ const Settings = () => {
     <div className='flex-1 overflow-auto relative bg-gray-900'>
         <main className='max-w-4xl mx-auto py-6 px-4 lg:px-8'>
             <Profile/>
+            <Notification/>
         </main>
     </div>
   )
